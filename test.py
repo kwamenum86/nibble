a4e053e2160de3cf185b0c3c58034aec2d4624e5
Kwame Thomison <kwamenum86@gmail.com>
2011-06-25 01:57:03 -0700

    First stab at testing this thing

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-import bytegen
+import os
+from nibble import Nibble
 
-writer = new Nibble('wbytes.bin')
+filepath = "wbytes.bin"
+
+def test_1():
+	"""Really weak test"""
+	n = Nibble(filepath)
+	data_good = True
+	n.write_items([9, 9, 9, 9], item_size=4)
+	n.close()
+	fh = open(filepath, "rb")
+	for byte in fh.read():
+		data_good &= ord(byte) == 153
+		if not data_good:
+			break
+	fh.close()
+	os.remove(filepath)
+	if data_good:
+		return "Success"
+	else:
+		return "Data corrupted"
+
+if __name__ == "__main__":
+	print "Performing test '%s':" % test_1.__doc__
+	print test_1() + "\n"
