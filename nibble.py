3e2c4019025251d9ccab893f27a835ba062051e4
Kwame Thomison <kwamenum86@gmail.com>
2011-06-26 16:02:23 -0700

    Optimize out expensize num_digits calls.  Just keep track of the buffer size instead.  Still one call to num_digits remaining.

--- nibble.py
+++ nibble.py
@@ -14,31 +14,38 @@ class Nibble():
 
 	def write_items(self, items, item_fn = None, item_size = None):
 		buffer = 1
+		buffer_size = 0
 		if self.fh is None or self.fh.closed:
 			self.fh = open(self.filename, "wb")
 		for item in items:
 			data = item_fn and item_fn(item) or item
 			for byte in Nibble.iter_bytes(data):
-				buffer <<= item_size or num_digits(byte, 2)
+				# TODO figure out how to get rid of num_digits here
+				shift_count = item_size or num_digits(byte, 2)
+				buffer <<= shift_count
+				buffer_size += shift_count
 				buffer |= byte
 				# Can't flush the buffer at 8 because of the dummy digit
-				digits = num_digits(buffer, 2)
-				if  digits > 8:
+				if  buffer_size > 7:
 					# extract digits 2 through 9 i.e. a byte's worth of bits offset by 1
 					# to account for the dummy digit
-					offset = digits - 9
+					offset = buffer_size - 8
 					out = (buffer & 255 << offset) >> offset
 					# Keep the remaining bits plus a dummy digit.  The first code path below
-					# is functionally equivalent to the second when digits == 9.
-					if digits == 9:
+					# is functionally equivalent to the second when buffer_size == 9.
+					if buffer_size == 8:
 						buffer = 1
+						buffer_size = 0
 					else:
 						# (255 >> (8 - offset)) gives us the number represented by
-						# *offset* ones digits.  For example, when offset is 2 output is 3
+						# *offset* ones digits.  For example, when offset is 2 output is 3.
+						# The statement below is meant to reduce the buffer to bits that
+						# have not been written yet.
 						buffer = buffer & (255 >> (8 - offset)) | (1 << offset)
+						buffer_size = offset
 					self.fh.write("%c" % out)
 		if buffer > 1:
-			buffer <<= 9 - num_digits(buffer, 2)
+			buffer <<= 9 - buffer_size
 			out = buffer & 255
 			self.fh.write("%c" % out)
 
