3dd67ab2b10cc3bfc9bfebc652989d516747d034
Kwame Thomison <kwamenum86@gmail.com>
2011-06-26 19:13:55 -0700

    Check the buffer size inside slice_buffer.  Instead of checking buffer size before calling slice_buffer check to see if out (returned from slice_buffer) is something useful before writing.

--- nibble.py
+++ nibble.py
@@ -13,22 +13,25 @@ class FileClient():
 
 	@staticmethod
 	def slice_buffer(buffer, buffer_size):
-		# extract digits 2 through 9 i.e. a byte's worth of bits offset by 1
-		# to account for the dummy digit
-		offset = buffer_size - 8
-		out = (buffer & 255 << offset) >> offset
-		# Keep the remaining bits plus a dummy digit.  The first code path below
-		# is functionally equivalent to the second when buffer_size == 9.
-		if buffer_size == 8:
-			buffer = 1
-			buffer_size = 0
-		else:
-			# (255 >> (8 - offset)) gives us the number represented by
-			# *offset* ones digits.  For example, when offset is 2 output is 3.
-			# The statement below is meant to reduce the buffer to bits that
-			# have not been written yet.
-			buffer &= (255 >> (8 - offset)) | (1 << offset)
-			buffer_size = offset
+		out = None
+		# Can't flush the buffer at 8 because of the dummy digit
+		if  buffer_size > 7:
+			# extract digits 2 through 9 i.e. a byte's worth of bits offset by 1
+			# to account for the dummy digit
+			offset = buffer_size - 8
+			out = (buffer & 255 << offset) >> offset
+			# Keep the remaining bits plus a dummy digit.  The first code path below
+			# is functionally equivalent to the second when buffer_size == 9.
+			if buffer_size == 8:
+				buffer = 1
+				buffer_size = 0
+			else:
+				# (255 >> (8 - offset)) gives us the number represented by
+				# *offset* ones digits.  For example, when offset is 2 output is 3.
+				# The statement below is meant to reduce the buffer to bits that
+				# have not been written yet.
+				buffer &= (255 >> (8 - offset)) | (1 << offset)
+				buffer_size = offset
 		return buffer, buffer_size, out
 
 	@staticmethod
@@ -51,9 +54,8 @@ class Writer(FileClient):
 			buffer <<= shift_count
 			buffer_size += shift_count
 			buffer |= byte
-			# Can't flush the buffer at 8 because of the dummy digit
-			if  buffer_size > 7:
-				buffer, buffer_size, out = FileClient.slice_buffer(buffer, buffer_size)
+			buffer, buffer_size, out = FileClient.slice_buffer(buffer, buffer_size)
+			if not out is None:
 				self.fh.write("%c" % out)
 		if buffer > 1:
 			buffer <<= 9 - buffer_size
