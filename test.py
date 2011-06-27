29efb589d30413d7a6d9c2240ec730e1e504e9bc
Kwame Thomison <kwamenum86@gmail.com>
2011-06-26 17:28:52 -0700

    Add test for the FileClient.file_func decorator.  Pull success and failure printing into the test_func decorator so functions don't need to worry about printing success/failure messages

--- test.py
+++ test.py
@@ -7,14 +7,15 @@ test_suite = []
 
 def test_func(fn):
 	def _fn():
-		print "Performing test '%s':" % fn.__doc__
-		print fn() + "\n"
+		print "Performing '%s' test:" % fn.__doc__
+		res = fn()
+		print (res and "Success" or "Failed") + "\n"
 	test_suite.append(_fn)
 	return _fn
 
 @test_func
-def test_1():
-	"""Really weak test"""
+def test():
+	"""Really weak"""
 	n = Writer(filepath)
 	data_good = True
 	n.write([9, 9, 9, 9], item_size=4)
@@ -26,10 +27,23 @@ def test_1():
 			break
 	fh.close()
 	os.remove(filepath)
-	if data_good:
-		return "Success"
-	else:
-		return "Data corrupted"
+	return data_good
+
+@test_func
+def test():
+	"""file_func"""
+	n = Writer(filepath)
+	byte_count = 0
+	n.write([9, 9], item_size=4)
+	n.close()
+	n.write([9, 9], item_size=4)
+	n.close()
+	fh = open(filepath, "rb")
+	for byte in fh.read():
+		byte_count += 1
+	fh.close()
+	os.remove(filepath)
+	return byte_count == 2
 
 if __name__ == "__main__":
 	for test in test_suite:
